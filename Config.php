159b8c706134452a15755efe55f676658654bbb3
mojko <mojko@495dd495-0106-db11-81b0-001320b0e4cc>
2009-12-22 11:41:47 +0000

    System constants configuration moved to a separate configuration file.
    git-svn-id: https://arcomage.net/svn/release@742 495dd495-0106-db11-81b0-001320b0e4cc

---
+++ Config.php
@@ -0,0 +1,25 @@
+<?php
+
+	/* -------------------------------- *
+	 * | MARCOMAGE CONFIGURATION FILE | *
+	 * -------------------------------- */
+
+	// constants
+	define("MAX_GAMES", 15);
+	define("DECK_SLOTS", 8);
+	define("BONUS_GAME_SLOTS", 4); // add bonus game slot every 4th level
+	define("BONUS_DECK_SLOTS", 6); // add bonus deck slot every 6th level
+	define("MESSAGE_LENGTH", 1000);
+	define("CHALLENGE_LENGTH", 250);
+	define("SYSTEM_NAME", "MArcomage"); // user name for system notification
+	define("NUM_THREADS", 4); // number of threads per section in the forum main page
+	define("THREADS_PER_PAGE", 30);
+	define("POSTS_PER_PAGE", 20);
+	define("POST_LENGTH", 4000);
+	define("PLAYERS_PER_PAGE", 50);
+	define("MESSAGES_PER_PAGE", 15);
+	define("CARDS_PER_PAGE", 20);
+	define("EFFECT_LENGTH", 255);
+	define("HOBBY_LENGTH", 300);
+
+?>
