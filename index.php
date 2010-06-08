29514359779ce8228027e2087433fddaff9aea7b
umage <umage@495dd495-0106-db11-81b0-001320b0e4cc>
2010-06-08 14:26:25 +0000

    Redirected errors to our own logfile, instead of writing to the central webserver error file. A new logfile will be created on a daily basis. Added /logs directory. Needs to be web-writable to work.
    git-svn-id: https://arcomage.net/svn/release@892 495dd495-0106-db11-81b0-001320b0e4cc

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+	// redirect errors to our own logfile
+	error_reporting(-1); // everything
+	ini_set("display_errors", "Off");
+	ini_set("error_log", "logs/arcomage-error-".strftime('%Y%m%d').".log");
+
 	// activate output buffering and optional compression
 	if ( stristr(@$_SERVER["HTTP_ACCEPT_ENCODING"],"gzip") && extension_loaded('zlib') && ini_get('zlib.output_compression') == 0 )
 		ob_start('ob_gzhandler', 16384);
