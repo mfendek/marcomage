5781b22e7267c80cbd419c7fffc0993e3e2ab7b9
mojko <mojko@495dd495-0106-db11-81b0-001320b0e4cc>
2010-06-12 20:13:10 +0000

    Corrected JQuery selector syntax.
    git-svn-id: https://arcomage.net/svn/release@898 495dd495-0106-db11-81b0-001320b0e4cc

--- javascript/utils.js
+++ javascript/utils.js
@@ -3,11 +3,11 @@
 $(document).ready(function() {
 
 	// sends in game chat message when ENTER key is hit
-	$("input[name=ChatMessage]").keypress(function(event) {
-		if (event.keyCode == '13') { event.preventDefault(); $("input[name=send_message]").click(); }
+	$("input[name='ChatMessage']").keypress(function(event) {
+		if (event.keyCode == '13') { event.preventDefault(); $("input[name='send_message']").click(); }
 	});
 
 	// blocks ENTER key to prevent section redirects
-	$("input[name!=ChatMessage][type!=password], select").keypress(function(event) { if (event.keyCode == '13') { event.preventDefault(); } });
+	$("input[name!='ChatMessage'][type!='password'], select").keypress(function(event) { if (event.keyCode == '13') { event.preventDefault(); } });
 
 });
