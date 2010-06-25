ef292d87ba3da5286ae8e8061387fed63e7483b7
mojko <mojko@495dd495-0106-db11-81b0-001320b0e4cc>
2010-06-25 07:55:23 +0000

    Player cleanup script corrected (now deletes users with inactivity longer than 12 weeks and with zero score).
    git-svn-id: https://arcomage.net/svn/release@949 495dd495-0106-db11-81b0-001320b0e4cc

--- scripts/players_cleanup.php
+++ scripts/players_cleanup.php
@@ -5,7 +5,7 @@
 	 * ---------------------------------------------- */
 	/*
 
-	- deletes dead users and their related data
+	- deletes dead users (no activity in more than 12 weeks and zero score) and their related data
 	- deletes 50 users at a time
 	- provides debug information about delete procedure
 
@@ -41,7 +41,7 @@
 
 	echo "Deleting player data..."."\n<br />\n<br />";
 
-	$result = $db->Query('SELECT `Username` FROM `logins` WHERE (UNIX_TIMESTAMP(`Last Query`) < UNIX_TIMESTAMP() - 60*60*24*7*3) LIMIT 0 , 50');
+	$result = $db->Query('SELECT `Username` FROM (SELECT `Username` FROM `logins` WHERE (UNIX_TIMESTAMP(`Last Query`) < UNIX_TIMESTAMP() - 60*60*24*7*12)) as `logins` INNER JOIN (SELECT `Username` FROM `scores` WHERE `Wins` + `Losses` + `Draws` = 0) as `scores` USING (`Username`) LIMIT 0 , 50');
 
 	while( $data = $result->Next() )
 	{
