378bb1f7b605614cca71c41102a9bff577720961
mojko <mojko@495dd495-0106-db11-81b0-001320b0e4cc>
2011-03-23 14:05:05 +0000

    Confirmatin added to admin actions.
    git-svn-id: https://arcomage.net/svn/release@1506 495dd495-0106-db11-81b0-001320b0e4cc

--- javascript/players.js
+++ javascript/players.js
@@ -9,4 +9,10 @@ $(document).ready(function() {
 		if (event.keyCode == '13') { event.preventDefault(); $("button[name='filter_players']").click(); }
 	});
 
+	// admin actions confirmation
+	$("button[name='change_access'], button[name='reset_password'], button[name='reset_avatar_remote'], button[name='reset_exp'], button[name='add_gold']").click(function() {
+		var str = new String();
+		return confirm(str.concat("Do you really want to ", $(this).html(), "?"));
+	});
+
 });
