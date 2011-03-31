bf28a6de121dbb6f1e7e02c879589aef98574083
mojko <mojko@495dd495-0106-db11-81b0-001320b0e4cc>
2011-03-31 13:11:31 +0000

    Replays cleanup script optimized.
    git-svn-id: https://arcomage.net/svn/release@1519 495dd495-0106-db11-81b0-001320b0e4cc

--- scripts/replays_cleanup.sql
+++ scripts/replays_cleanup.sql
@@ -8,19 +8,12 @@ SET AUTOCOMMIT=0;
 
 START TRANSACTION;
 
--- copy data structure
-CREATE TABLE `replays_tmp` LIKE `replays_data`;
+-- delete replays data
+DELETE `replays_data` FROM `replays_head` INNER JOIN `replays_data` USING (`GameID`) WHERE `Views` = 0 AND `EndType` != "Pending" AND `Finished` < NOW() - INTERVAL 2 DAY;
 
--- move replay data that will be preserved to temporary table (replays with positive views, unfinished replays and replays that are younger than 2 days)
-INSERT INTO `replays_tmp` SELECT `GameID`, `Turn`, `Current`, `Round`, `Data` FROM (SELECT `GameID` FROM `replays_head` WHERE `Views` > 0 OR `EndType` = "Pending" OR `Finished` > NOW() - INTERVAL 2 DAY) as `head` INNER JOIN `replays_data` USING (`GameID`);
-
--- delete old table along with data that doesn't need to be preserved
-DROP TABLE `replays_data`;
-
--- rename temporary table
-RENAME TABLE `replays_tmp` TO `replays_data`;
+OPTIMIZE TABLE `replays_data`;
 
 -- update replays_head table about recent data deletion
-UPDATE `replays_head` SET `Deleted` = 1 WHERE `Deleted` = 0 AND NOT (`Views` > 0 OR `EndType` = "Pending" OR `Finished` > NOW() - INTERVAL 2 DAY);
+UPDATE `replays_head` SET `Deleted` = 1 WHERE `Deleted` = 0 AND `Views` = 0 AND `EndType` != "Pending" AND `Finished` < NOW() - INTERVAL 2 DAY;
 
 COMMIT;
