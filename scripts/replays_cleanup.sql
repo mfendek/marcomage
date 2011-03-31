451862b907a45a7c9d97648442f6607b8dd1939e
umage <umage@495dd495-0106-db11-81b0-001320b0e4cc>
2011-03-31 13:17:57 +0000

    Removed transaction from replays cleanup script to speed up the process (and partial results of a failed execution are okay too). Removed optimize table query, it should be done in a separate maintenance step.
    git-svn-id: https://arcomage.net/svn/release@1520 495dd495-0106-db11-81b0-001320b0e4cc

--- scripts/replays_cleanup.sql
+++ scripts/replays_cleanup.sql
@@ -4,16 +4,8 @@
 -- deletes replays data that belong to finished replays with 0 views and are older than 2 days
 --
 
-SET AUTOCOMMIT=0;
-
-START TRANSACTION;
-
 -- delete replays data
 DELETE `replays_data` FROM `replays_head` INNER JOIN `replays_data` USING (`GameID`) WHERE `Views` = 0 AND `EndType` != "Pending" AND `Finished` < NOW() - INTERVAL 2 DAY;
 
-OPTIMIZE TABLE `replays_data`;
-
 -- update replays_head table about recent data deletion
 UPDATE `replays_head` SET `Deleted` = 1 WHERE `Deleted` = 0 AND `Views` = 0 AND `EndType` != "Pending" AND `Finished` < NOW() - INTERVAL 2 DAY;
-
-COMMIT;
