ed0cc169598cafca18551583ef2774094e12fdc4
mojko <mojko@495dd495-0106-db11-81b0-001320b0e4cc>
2011-04-09 12:38:28 +0000

    Error logging added to AJAXhandler.
    git-svn-id: https://arcomage.net/svn/release@1544 495dd495-0106-db11-81b0-001320b0e4cc

--- AJAXhandler.php
+++ AJAXhandler.php
@@ -4,6 +4,10 @@
 */
 ?>
 <?php
+	// redirect errors to our own logfile
+	error_reporting(-1); // everything
+	ini_set("display_errors", "Off");
+	ini_set("error_log", "logs/arcomage-error-".strftime('%Y%m%d').".log");
 
 	do { // dummy scope
 
