5ce2568fa3e626c95faeb16108e15d4e955e13e0
umage <umage@495dd495-0106-db11-81b0-001320b0e4cc>
2011-04-25 17:12:11 +0000

    Replaced php-level output buffering/compression with apache-level htaccess AddOutputFilterByType directive (requires AllowOverride FileInfo). Now auxiliary files (css, javascript, xml...) will be compressed as well.
    git-svn-id: https://arcomage.net/svn/release@1563 495dd495-0106-db11-81b0-001320b0e4cc

--- index.php
+++ index.php
@@ -4,12 +4,6 @@
 	ini_set("display_errors", "Off");
 	ini_set("error_log", "logs/arcomage-error-".strftime('%Y%m%d').".log");
 
-	// activate output buffering and optional compression
-	if ( stristr(@$_SERVER["HTTP_ACCEPT_ENCODING"],"gzip") && extension_loaded('zlib') && ini_get('zlib.output_compression') == 0 )
-		ob_start('ob_gzhandler', 16384);
-	else
-		ob_start();
-
 	// enable xhtml+xml mode if the client supports it
 	if ( stristr(@$_SERVER["HTTP_ACCEPT"],"application/xhtml+xml") )
 		header("Content-type: application/xhtml+xml");
@@ -17,6 +11,4 @@
 		header("Content-type: text/html");
 
 	require_once("main.php");
-
-	ob_end_flush();
 ?>
