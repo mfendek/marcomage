54ca411d76f44a0ef35bbe16f9d5552f234fbd4d
umage <umage@495dd495-0106-db11-81b0-001320b0e4cc>
2012-09-26 18:05:36 +0000

    Decreased indent in CDatabase.php. Compacted the header.
    git-svn-id: https://arcomage.net/svn/release@1700 495dd495-0106-db11-81b0-001320b0e4cc

--- CDatabase.php
+++ CDatabase.php
@@ -1,146 +1,141 @@
 <?php
-/*
-	CDatabase - encapsulated MySQL database access (wrapped PDO)
-*/
-?>
-<?php
-	class CDatabase
+class CDatabase // encapsulated MySQL database access (PDO)
+{
+	private $db = false;
+	public $status = 'ERROR_DB_OFFLINE';
+	public $queries = 0; // counter
+	public $qtime = 0; // time spent
+	public $log = array(); // query log
+
+	public function __construct($server, $username, $password, $database)
 	{
-		private $db = false;
-		public $status = 'ERROR_DB_OFFLINE';
-		public $queries = 0; // counter
-		public $qtime = 0; // time spent
-		public $log = array(); // query log
+		$dsn = 'mysql:dbname='.$database.';host='.$server.'';
 
-		public function __construct($server, $username, $password, $database)
-		{
-			$dsn = 'mysql:dbname='.$database.';host='.$server.'';
+		$options = array(
+			PDO::ATTR_PERSISTENT => true,
+			PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+		);
 
-			$options = array(
-				PDO::ATTR_PERSISTENT => true,
-				PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-			);
+		try
+			{ $db = new PDO($dsn, $username, $password, $options); }
+		catch (PDOException $e)
+			{ $this->status = $e->getMessage(); return; }
 
-			try
-				{ $db = new PDO($dsn, $username, $password, $options); }
-			catch (PDOException $e)
-				{ $this->status = $e->getMessage(); return; }
-
-			$this->db = $db;
-			$this->status = 'SUCCESS';
-			return;
-		}
-
-		public function isOnline()
-		{
-			return ($this->db) ? true : false;
-		}
-
-		public function LastID()
-		{
-			return $this->db->lastInsertId();
-		}
-
-		public function BeginTransaction()
-		{
-			return $this->db->beginTransaction();
-		}
-
-		public function RollBack()
-		{
-			return $this->db->rollBack();
-		}
-
-		public function Commit()
-		{
-			return $this->db->commit();
-		}
-
-		///
-		/// Executes database query with provided parameter values
-		/// @param string $query database query
-		/// @param array $params parameter values in the order they appear in the query
-		/// @return array result data if the operation succeeds (empty array in case of non-SELECT statements), false if it fails
-		public function Query($query, array $params = array())
-		{
-			if (!$this->db) { $this->status = 'ERROR_DB_OFFLINE'; return false; };
-
-			$t_start = microtime(TRUE);
-
-			// create new PDOStatement
-			try
-				{ $statement = $this->db->prepare($query); }
-			catch (PDOException $e)
-				{ $this->status = 'ERROR_QUERY: '.$e->getMessage(); return false; }
-
-			$result = $statement->execute($params);
-
-			$t_end = microtime(TRUE);
-			if (!$result) { $this->status = 'ERROR_QUERY: '.implode(" ", $statement->errorInfo()); return false; }
-
-			$this->queries++;
-			$this->qtime += $t_end - $t_start;
-			$this->log[] = sprintf("[%.2f ms] %s", round(1000*($t_end - $t_start),2), $query);
-			$this->status = 'SUCCESS';
-
-			// get result data
-			$data = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-			// free statement object
-			$statement = null;
-
-			return $data;
-		}
-
-		///
-		/// Executes prepared database query multiple times with provided parameter values
-		/// each run of query has its own set of parameters
-		/// all queries are executed as a single transaction
-		/// the key under which the parameter set is located in the $params array is used in $results array for matching query result
-		/// @param string $query database query
-		/// @param array $params sequence of sets of parameter values in the order they appear in the query
-		/// @return array result data for each query if the operation succeeds, false if it fails
-		public function MultiQuery($query, array $params)
-		{
-			if (!$this->db) { $this->status = 'ERROR_DB_OFFLINE'; return false; }
-			if (count($params) == 0) { $this->status = 'ERROR_PARAMS_EMPTY'; return false; }
-
-			$t_start = microtime(TRUE);
-
-			// create new PDOStatement
-			try
-				{ $statement = $this->db->prepare($query); }
-			catch (PDOException $e)
-				{ $this->status = 'ERROR_QUERY: '.$e->getMessage(); return false; }
-
-			$this->db->beginTransaction();
-			$results = array();
-
-			foreach ($params as $key => $param_set)
-			{
-				$result = $statement->execute($param_set);
-				if (!$result)
-				{
-					$this->status = 'ERROR_QUERY: '.implode(" ", $statement->errorInfo());
-					$this->db->rollBack();
-
-					return false;
-				}
-				// store result data
-				$results[$key] = $statement->fetchAll(PDO::FETCH_ASSOC);
-			}
-			$this->db->commit();
-
-			$t_end = microtime(TRUE);
-			$this->queries++;
-			$this->qtime += $t_end - $t_start;
-			$this->log[] = sprintf("[%.2f ms] %s", round(1000*($t_end - $t_start),2), $query);
-			$this->status = 'SUCCESS';
-
-			// free statement object
-			$statement = null;
-
-			return $results;
-		}
+		$this->db = $db;
+		$this->status = 'SUCCESS';
+		return;
 	}
+
+	public function isOnline()
+	{
+		return ($this->db) ? true : false;
+	}
+
+	public function LastID()
+	{
+		return $this->db->lastInsertId();
+	}
+
+	public function BeginTransaction()
+	{
+		return $this->db->beginTransaction();
+	}
+
+	public function RollBack()
+	{
+		return $this->db->rollBack();
+	}
+
+	public function Commit()
+	{
+		return $this->db->commit();
+	}
+
+	///
+	/// Executes database query with provided parameter values
+	/// @param string $query database query
+	/// @param array $params parameter values in the order they appear in the query
+	/// @return array result data if the operation succeeds (empty array in case of non-SELECT statements), false if it fails
+	public function Query($query, array $params = array())
+	{
+		if (!$this->db) { $this->status = 'ERROR_DB_OFFLINE'; return false; };
+
+		$t_start = microtime(TRUE);
+
+		// create new PDOStatement
+		try
+			{ $statement = $this->db->prepare($query); }
+		catch (PDOException $e)
+			{ $this->status = 'ERROR_QUERY: '.$e->getMessage(); return false; }
+
+		$result = $statement->execute($params);
+
+		$t_end = microtime(TRUE);
+		if (!$result) { $this->status = 'ERROR_QUERY: '.implode(" ", $statement->errorInfo()); return false; }
+
+		$this->queries++;
+		$this->qtime += $t_end - $t_start;
+		$this->log[] = sprintf("[%.2f ms] %s", round(1000*($t_end - $t_start),2), $query);
+		$this->status = 'SUCCESS';
+
+		// get result data
+		$data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+		// free statement object
+		$statement = null;
+
+		return $data;
+	}
+
+	///
+	/// Executes prepared database query multiple times with provided parameter values
+	/// each run of query has its own set of parameters
+	/// all queries are executed as a single transaction
+	/// the key under which the parameter set is located in the $params array is used in $results array for matching query result
+	/// @param string $query database query
+	/// @param array $params sequence of sets of parameter values in the order they appear in the query
+	/// @return array result data for each query if the operation succeeds, false if it fails
+	public function MultiQuery($query, array $params)
+	{
+		if (!$this->db) { $this->status = 'ERROR_DB_OFFLINE'; return false; }
+		if (count($params) == 0) { $this->status = 'ERROR_PARAMS_EMPTY'; return false; }
+
+		$t_start = microtime(TRUE);
+
+		// create new PDOStatement
+		try
+			{ $statement = $this->db->prepare($query); }
+		catch (PDOException $e)
+			{ $this->status = 'ERROR_QUERY: '.$e->getMessage(); return false; }
+
+		$this->db->beginTransaction();
+		$results = array();
+
+		foreach ($params as $key => $param_set)
+		{
+			$result = $statement->execute($param_set);
+			if (!$result)
+			{
+				$this->status = 'ERROR_QUERY: '.implode(" ", $statement->errorInfo());
+				$this->db->rollBack();
+
+				return false;
+			}
+			// store result data
+			$results[$key] = $statement->fetchAll(PDO::FETCH_ASSOC);
+		}
+		$this->db->commit();
+
+		$t_end = microtime(TRUE);
+		$this->queries++;
+		$this->qtime += $t_end - $t_start;
+		$this->log[] = sprintf("[%.2f ms] %s", round(1000*($t_end - $t_start),2), $query);
+		$this->status = 'SUCCESS';
+
+		// free statement object
+		$statement = null;
+
+		return $results;
+	}
+};
 ?>
