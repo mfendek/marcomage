7642278ff53acf8440d0815fc88bd5c4d7b92361
umage <umage@495dd495-0106-db11-81b0-001320b0e4cc>
2012-10-03 08:25:38 +0000

    Accidental commit of dev environment src edit (from r1702).
    git-svn-id: https://arcomage.net/svn/release@1709 495dd495-0106-db11-81b0-001320b0e4cc

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	// redirect errors to our own logfile
 	error_reporting(-1); // everything
-//	ini_set("display_errors", "Off");
+	ini_set("display_errors", "Off");
 	ini_set("error_log", "logs/arcomage-error-".strftime('%Y%m%d').".log");
 
 	// enable xhtml+xml mode if the client supports it
