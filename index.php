7f3e383a1ec986fa61077d05bcf358c4e8c98e46
mojko <mojko@495dd495-0106-db11-81b0-001320b0e4cc>
2014-01-02 12:40:54 +0000

    Header processing moved later in the code to prevent header conflicts (there are actions that have custom header and don't want to use the general one).
    git-svn-id: https://arcomage.net/svn/release@1965 495dd495-0106-db11-81b0-001320b0e4cc

--- index.php
+++ index.php
@@ -4,11 +4,5 @@
 //	ini_set("display_errors", "Off");
 	ini_set("error_log", "logs/arcomage-error-".strftime('%Y%m%d').".log");
 
-	// enable xhtml+xml mode if the client supports it
-	if ( stristr(@$_SERVER["HTTP_ACCEPT"],"application/xhtml+xml") )
-		header("Content-type: application/xhtml+xml");
-	else
-		header("Content-type: text/html");
-
 	require_once("main.php");
 ?>
--- main.php
+++ main.php
@@ -5115,6 +5115,12 @@ default:
 	$module = 'templates/template_'.strtolower($section_name).'.xsl';
 	$params["main"]["subsection"] = (isset($subsection_name)) ? $subsection_name : "";
 
+	// HTML header - enable xhtml+xml mode if the client supports it
+	if ( stristr(@$_SERVER["HTTP_ACCEPT"],"application/xhtml+xml") )
+		header("Content-type: application/xhtml+xml");
+	else
+		header("Content-type: text/html");
+
 	// HTML code generation
 
 	$querytime_end = microtime(TRUE);
