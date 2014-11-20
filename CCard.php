5387cd9d88a5c4b4c3f6912e7307cfae69be085e
mojko <mojko@495dd495-0106-db11-81b0-001320b0e4cc>
2014-11-20 13:12:28 +0000

    Card filter processing moved from XSLT to PHP, which is much faster. Some card data is now optional and can be omitted.
    git-svn-id: https://arcomage.net/svn/release@2015 495dd495-0106-db11-81b0-001320b0e4cc

--- CCard.php
+++ CCard.php
@@ -31,271 +31,355 @@
 		{
 			return new CCard($cardid, $this);
 		}
-		
-		/**
-		 * Constructs a filtering query based on the specified filters.
-		 * Available filters are:
-		 * <ul>
-		 * <li> 'name'     => { <search_substring> }, queries `Name` </li>
-		 * <li> 'class'    => { None | Common | Uncommon | Rare }, queries `Class` </li>
-		 * <li> 'keyword'  => { Any keyword | No keywords | <a specific keyword> }, queries `Keywords` </li>
-		 * <li> 'cost'     => { Red | Blue | Green | Zero | Mixed }, queries `Bricks`, `Gems` and `Recruits` </li>
-		 * <li> 'advanced' => { <a specific substring> }, queries `Effect` </li>
-		 * <li> 'support'  => { Any keyword | No keywords | <a specific keyword> }, queries `Effect` </li>
-		 * <li> 'level'    => { <specific_level> }, queries `level` </li>
-		 * <li> 'level_op' => { = | <= }, additional parameter for `level` defaults to '=' </li>
-		 * </ul>
-		 * @param array $filters an array of chosen filters and their parameters
-		 * @return string a boolean expression to be used in a card retrieval query
-		*/
-		private function makeFilterquery(array $filters)
+
+		///
+		/// Filters cards according to the provided filtering instructions.
+		/// @param array $filters (optional) an array of chosen filters and their parameters
+		/// Available filters are:
+		/// <ul>
+		/// <li> 'name'     => { <search_substring> }, queries `Name` </li>
+		/// <li> 'class'    => { None | Common | Uncommon | Rare }, queries `Class` </li>
+		/// <li> 'keyword'  => { Any keyword | No keywords | <a specific keyword> }, queries `Keywords` </li>
+		/// <li> 'cost'     => { Red | Blue | Green | Zero | Mixed }, queries `Bricks`, `Gems` and `Recruits` </li>
+		/// <li> 'advanced' => { <a specific substring> }, queries `Effect` </li>
+		/// <li> 'support'  => { Any keyword | No keywords | <a specific keyword> }, queries `Effect` </li>
+		/// <li> 'level'    => { <specific_level> }, queries `level` </li>
+		/// <li> 'level_op' => { = | <= }, additional parameter for `level` defaults to '=' </li>
+		/// </ul>
+		/// @return array ids for cards that match the filters
+		public function getList(array $filters = array())
 		{
-			$query = "@id > 0"; // sentinel
+			// list all cards
+			$cards = $this->getData();
 
-			if( isset($filters['name']) )
-			{
-				$query .= " and ";
-				$query .= "contains(am:name, '".preg_replace("/[^a-zA-Z0-9 ]/i", '', $filters['name'])."')"; // case-sensitive search
-			}
+			// remove default from cards
+			unset($cards[0]);
 
-			if( isset($filters['class']) )
-			{
-				$query .= " and ";
-				$query .= "am:class = '".$filters['class']."'"; //FIXME: no escaping
-			}
-
-			if( isset($filters['keyword']) )
-			{
-				$query .= " and ";
-				switch( $filters['keyword'] )
-				{
-				case 'Any keyword': $query .= "am:keywords != ''"; break;
-				case 'No keywords': $query .= "am:keywords = ''"; break;
-				default           : $query .= "contains(am:keywords, '".$filters['keyword']."')"; break; //FIXME: no escaping
+			// filter cards
+			$out = array();
+			foreach ($cards as $cardId => $card) {
+				// name filter
+				if (isset($filters['name']) and strpos($card['name'], $filters['name']) === false) {
+					continue;
 				}
-			}
 
-			if( isset($filters['cost']) )
-			{
-				$query .= " and ";
-				switch( $filters['cost'] )
-				{
-				case 'Red'  : $query .= "am:cost/am:gems = 0 and am:cost/am:recruits = 0 and am:cost/am:bricks > 0"; break;
-				case 'Blue' : $query .= "am:cost/am:recruits = 0 and am:cost/am:bricks = 0 and am:cost/am:gems > 0"; break;
-				case 'Green': $query .= "am:cost/am:gems = 0 and am:cost/am:bricks = 0 and am:cost/am:recruits > 0"; break;
-				case 'Zero' : $query .= "am:cost/am:bricks = 0 and am:cost/am:gems = 0 and am:cost/am:recruits = 0"; break;
-				case 'Mixed': $query .= "(am:cost/am:bricks > 0) + (am:cost/am:gems > 0) + (am:cost/am:recruits > 0) >= 2"; break;
-				default     : $query .= "true"; //FIXME: should never happen
+				// rarity filter
+				if (isset($filters['class']) and $card['class'] != $filters['class']) {
+					continue;
 				}
-			}
 
-			if( isset($filters['advanced']) )
-			{
-				$query .= " and ";
-				$query .= "(contains(am:effect, '".$filters['advanced']."') or contains(am:effect, '".strtolower($filters['advanced'])."'))"; //FIXME: no escaping
-			}
-
-			if( isset($filters['support']) )
-			{
-				$query .= " and ";
-				// TODO find a better way to look for keywords in the effect (we are now searching for "<b>", becuase every keyword has them)
-				switch( $filters['support'] )
-				{
-				case 'Any keyword': $query .= "contains(am:effect, '<b>')"; break;
-				case 'No keywords': $query .= "not(contains(am:effect, '<b>'))"; break;
-				default           : $query .= "contains(am:effect, '<b>".$filters['support']."')"; //FIXME: no escaping
+				// keyword filter
+				if (isset($filters['keyword'])) {
+					// case 1: any keywords
+					if ($filters['keyword'] == 'Any keyword') {
+						if ($card['keywords'] == '') {
+							continue;
+						}
+					}
+					// case 2: no keywords
+					elseif ($filters['keyword'] == 'No keywords') {
+						if ($card['keywords'] != '') {
+							continue;
+						}
+					}
+					// case 3: one specific keyword
+					else {
+						if (strpos($card['keywords'], $filters['keyword']) === false) {
+							continue;
+						}
+					}
 				}
+
+				// resource cost filter
+				if (isset($filters['cost'])) {
+					// case 1: bricks cost only
+					if ($filters['cost'] == 'Red') {
+						if ($card['bricks'] == 0 or $card['gems'] > 0 or $card['recruits'] > 0) {
+							continue;
+						}
+					}
+					// case 2: gems cost only
+					elseif ($filters['cost'] == 'Blue') {
+						if ($card['bricks'] > 0 or $card['gems'] == 0 or $card['recruits'] > 0) {
+							continue;
+						}
+					}
+					// case 3: recruits cost only
+					elseif ($filters['cost'] == 'Green') {
+						if ($card['bricks'] > 0 or $card['gems'] > 0 or $card['recruits'] == 0) {
+							continue;
+						}
+					}
+					// case 4: zero cost only
+					elseif ($filters['cost'] == 'Zero') {
+						if ($card['bricks'] > 0 or $card['gems'] > 0 or $card['recruits'] > 0) {
+							continue;
+						}
+					}
+					// case 5: mixed cost only
+					elseif ($filters['cost'] == 'Mixed') {
+						if ((($card['bricks'] > 0) + ($card['gems'] > 0) + ($card['recruits'] > 0)) < 2) {
+							continue;
+						}
+					}
+				}
+
+				// advanced filter
+				if (isset($filters['advanced']) and strpos($card['effect'], $filters['advanced']) === false and strpos($card['effect'], strtolower($filters['advanced'])) === false) {
+					continue;
+				}
+
+				// support keyword filter
+				if (isset($filters['support'])) {
+					// case 1: any keywords
+					if ($filters['support'] == 'Any keyword') {
+						if (strpos($card['effect'], '<b>') === false) {
+							continue;
+						}
+					}
+					// case 2: no keywords
+					elseif ($filters['support'] == 'No keywords') {
+						if (strpos($card['effect'], '<b>') !== false) {
+							continue;
+						}
+					}
+					// case 3: one specific keyword
+					else {
+						if (strpos($card['effect'], '<b>'.$filters['support']) === false) {
+							continue;
+						}
+					}
+				}
+
+				// date created filter
+				if (isset($filters['created']) and $card['created'] != $filters['created']) {
+					continue;
+				}
+
+				// date modified filter
+				if (isset($filters['modified']) and $card['modified'] != $filters['modified']) {
+					continue;
+				}
+
+				// level filter
+				if (isset($filters['level'])) {
+					// determine operator
+					$operator = (isset($filters['level_op']) and in_array($filters['level_op'], array('=', '<='))) ? $filters['level_op'] : '<=';
+
+					// case 1: equal
+					if ($operator == '=') {
+						if ($card['level'] != $filters['level']) {
+							continue;
+						}
+					}
+					// case 2: default operator (less or equal)
+					else {
+						if ($card['level'] > $filters['level']) {
+							continue;
+						}
+					}
+				}
+
+				$out[] = $cardId;
 			}
 
-			if( isset($filters['created']) )
-			{
-				$query .= " and am:created = '".$filters['created']."'";
-			}
-
-			if( isset($filters['modified']) )
-			{
-				$query .= " and am:modified = '".$filters['modified']."'";
-			}
-
-			if( isset($filters['level']) )
-			{
-				$operator = (isset($filters['level_op']) and in_array($filters['level_op'], array('=', '<='))) ? $filters['level_op'] : '<=';
-				$query .= " and ";
-				$query .= "am:level ".$operator." ".$filters['level']; //FIXME: no escaping
-			}
-			return $query;
+			return $out;
 		}
 
-		/**
-		 * Filters cards according to the provided filtering instructions.
-		 * @see CCards::makeFilterquery()
-		 * @param array $filters an array of chosen filters and their parameters
-		 * @return array an array of ids for cards that match the filters
-		*/
-		public function getList(array $filters)
+		///
+		/// Calculate number of pages for current card list (specified by filters)
+		/// @param array $filters filters
+		/// @return int pages if operation was successful, false otherwise
+		public function countPages(array $filters)
 		{
-			$db = $this->getDB();
-
-			$result = $db->xpath("/am:cards/am:card[".$this->makeFilterquery($filters)."]/@id");
-			
-			if( $result === false ) return array(); // workaround for http://bugs.php.net/bug.php?id=48601
-			
-			$cards = array();
-			foreach( $result as $card )
-				$cards[] = (int)$card;
-			
-			return $cards;
-		}
-
-		public function countPages(array $filters) // calculate number of pages for current card list (specified by filters)
-		{
-			$db = $this->getDB();
-
-			$result = $db->xpath("/am:cards/am:card[".$this->makeFilterquery($filters)."]/@id");
-
-			if( $result === false ) return array(); // workaround for http://bugs.php.net/bug.php?id=48601
+			$result = $this->getList($filters);
+			if ($result === false) {
+				return array(); // workaround for http://bugs.php.net/bug.php?id=48601
+			}
 
 			return ceil(count($result) / CARDS_PER_PAGE);
 		}
 
-		/**
-		 * Retrieves data for the specified card ids.
-		 * Can be used in combination with CCards::getList().
-		 * The same card id may be specified multiple times.
-		 * The result will use the same keys and key order as the input.
-		 * @param array $ids an array of card ids to retrieve
-		 * @param int $page current page number (optional parameter)
-		 * @return array an array of the requested cards' data
-		*/
-		public function getData(array $ids, $page = -1)
+		///
+		/// Retrieves data for the specified card ids.
+		/// Can be used in combination with Cards::GetList().
+		/// The same card id may be specified multiple times.
+		/// The result will use the same keys and key order as the input.
+		/// @param array $ids an array of card ids to retrieve
+		/// @return array an array of the requested cards' data
+		public function getData(array $ids = array())
 		{
-			$db = $this->getDB();
-
 			// initialize on first use
-			if( $this->cache === false )
-			{
+			if ($this->cache === false) {
+				$db = $this->getDb();
+
 				// since xpath is too slow for this task, just grab everything and process it in php
-				$cards = $db->xpath("/am:cards/am:card");
-				if( $cards === false ) return false;
-				
-				$this->cache = array();
-				foreach( $cards as $card )
-				{
+				$cards = $db->xpath('/am:cards/am:card');
+				if ($cards === false) {
+					return false;
+				}
+
+				$this->cache = $default = array();
+				foreach ($cards as $card) {
+					$data = array();
+
+					// mandatory data
 					$data['id']       = (int)$card->attributes()->id;
 					$data['name']     = (string)$card->name;
 					$data['class']    = (string)$card->class;
-					$data['bricks']   = (int)$card->cost->bricks;
-					$data['gems']     = (int)$card->cost->gems;
-					$data['recruits'] = (int)$card->cost->recruits;
-					$data['modes']    = (int)$card->modes;
-					$data['level']    = (int)$card->level;
-					$data['keywords'] = (string)$card->keywords;
+
+					// optional data
+					foreach (['bricks', 'gems', 'recruits'] as $resource) {
+						if (isset($card->cost->$resource)) {
+							$data[$resource] = (int)$card->cost->$resource;
+						}
+					}
+
+					if (isset($card->modes)) {
+						$data['modes'] = (int)$card->modes;
+					}
+
+					$data['level'] = (int)$card->level;
+
+					if (isset($card->keywords)) {
+						$data['keywords'] = (string)$card->keywords;
+					}
+
 					$data['effect']   = (string)$card->effect;
 					$data['code']     = (string)$card->code;
 					$data['created']  = (string)$card->created;
 					$data['modified'] = (string)$card->modified;
+
+					// case 1: default card - store default data
+					if ($data['id'] == 0) {
+						$default = $data;
+					}
+					// case 2: standard card - merge data with default
+					else {
+						$data = array_merge($default, $data);
+					}
+
 					$this->cache[$data['id']] = $data;
 				}
 			}
 
-			$cards = $this->cache;
-			$out = $names = array();
-			foreach( $ids as $index => $id )
-			{
-				if( !isset($cards[$id]) )
-					return NULL; // nonexistent card
-				
-				$out[$index] = $cards[$id];
-				if ($page > -1) $names[$cards[$id]['name']] = $id; // map card names to ids
+			// return all cards in case no IDs are specified
+			if (count($ids) == 0) {
+				return $this->cache;
 			}
 
-			if ($page > -1) // retrieve current page of the card list
-			{
-				$out = array();
-				ksort($names, SORT_STRING);
-				$names = array_slice($names, $page * CARDS_PER_PAGE, CARDS_PER_PAGE);
-				foreach ($names as $card_id) $out[] = $cards[$card_id];
+			// match card data with specified card ids
+			$cards = $this->cache;
+			$out = $names = array();
+			foreach ($ids as $index => $id) {
+				// check if specified card has matching data
+				if (!isset($cards[$id])) {
+					return false; // nonexistent card
+				}
+
+				$out[$index] = $cards[$id];
 			}
 
 			return $out;
 		}
 		
-		// returns distinct levels that are less or equal to specified level
+		///
+		/// Returns distinct levels that are less or equal to specified level which are present in the card database
+		/// @param int $level (optional) level
+		/// @return array levels if operation was successful, false otherwise
 		public function levels($level = -1)
 		{
-			$levels = array();
-			
-			$cond = ($level >= 0) ? "[am:level <= ".$level."]" : '';
-			
-			$db = $this->getDB();
-			$result = $db->xpath("/am:cards/am:card".$cond."/am:level");
-			if( $result === false ) return $levels;
+			$filter = ($level >= 0) ? array('level' => $level) : array();
 
-			foreach($result as $entry)
-			{
-				$card_level = (int)$entry;
+			$result = $this->getData($this->getList($filter));
+			if ($result === false) {
+				return array();
+			}
+
+			$levels = array();
+			foreach ($result as $card) {
+				$card_level = $card['level'];
 				$levels[$card_level] = $card_level;
 			}
-			
+
 			sort($levels);
 
 			return $levels;
 		}
 		
-		// returns all distinct keywords
+		///
+		/// Returns all distinct keywords
+		/// @return array keywords if operation was successful, false otherwise
 		public function keywords()
 		{
+			$result = $this->getData($this->getList(['keyword' => 'Any keyword']));
+			if ($result === false) {
+				return array();
+			}
+
 			$keywords = array();
-			
-			$db = $this->getDB();
-			$result = $db->xpath("/am:cards/am:card[am:keywords != '']/am:keywords");
-			if( $result === false ) return $keywords;
-			
-			foreach($result as $entry)
-			{
-				$words = explode(",", $entry); // split individual keywords
-				foreach($words as $word)
-				{
-					$word = preg_split("/ \(/", $word, 0); // remove parameter if present
+			foreach ($result as $card) {
+				$entry = $card['keywords'];
+
+				// split individual keywords
+				$words = explode(",", $entry);
+
+				foreach ($words as $word) {
+					// remove keyword parameter if present
+					$word = preg_split("/ \(/", $word, 0);
 					$word = $word[0];
-					$keywords[$word] = $word; // removes duplicates
+
+					// remove duplicates
+					$keywords[$word] = $word;
 				}
 			}
 
 			sort($keywords);
-			
+
 			return $keywords;
 		}
 		
-		public function listCreationDates() // returns list of distinct creation dates
+		///
+		/// Returns list of distinct creation dates present in the card database
+		/// @return array dates if operation was successful, false otherwise
+		public function listCreationDates()
 		{
-			$db = $this->getDB();
-			
-			$result = $db->xpath('/am:cards/am:card[@id > 0]/am:created');
-			if( $result === false ) return false;
+			$result = $this->getData($this->getList());
+			if ($result === false) {
+				return false;
+			}
+
 			$dates = array();
-			
-			foreach($result as $created) $dates[] = (string)$created;
-			
+			foreach ($result as $card) {
+				$dates[] = $card['created'];
+			}
+
+			// remove duplicates
 			$dates = array_unique($dates);
 			rsort($dates);
-			
+
 			return $dates;
 		}
 		
-		public function listModifyDates() // returns list of distinct modification dates
+		///
+		/// Returns list of distinct modification dates present in the card database
+		/// @return array dates if operation was successful, false otherwise
+		public function listModifyDates()
 		{
-			$db = $this->getDB();
-			
-			$result = $db->xpath('/am:cards/am:card[@id > 0]/am:modified');
-			if( $result === false ) return false;
+			$result = $this->getData($this->getList());
+			if ($result === false) {
+				return false;
+			}
+
 			$dates = array();
-			
-			foreach($result as $modified) $dates[] = (string)$modified;
-			
+			foreach ($result as $card) {
+				$dates[] = $card['modified'];
+			}
+
+			// remove duplicates
 			$dates = array_unique($dates);
 			rsort($dates);
-			
+
 			return $dates;
 		}
 		
